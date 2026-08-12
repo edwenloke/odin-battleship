a54b9512f8e7bffcd0b294ae1a424d6d5eded29f
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-08-13 02:38:38 +0800

    feat: add GameBoard class and tests

---
+++ src/gameLogic/gameboard.js
@@ -0,0 +1,105 @@
+class GameBoard {
+  constructor() {
+    this.marked = [];
+    this.occupied = [];
+    this.ships = new Map();
+  }
+
+  //   Get random coordinate---------------------------------------------------------------------
+  rollCoordinate(length) {
+    // Horizontal placement-----------------------------------
+    // Max starting point at x
+    const maxX = 10 - length;
+    // Get random num between 0 - maxX
+    let x = Math.floor(Math.random() * (maxX + 1));
+    // Get random num for y(0 - 9)
+    const y = Math.floor(Math.random() * 10);
+
+    // Horizontal coordinate
+    const horizontal = [];
+    while (horizontal.length < length) {
+      horizontal.push([x, y]);
+      x++;
+    }
+
+    // Vertical placement----------------------------------------
+    const maxY = 10 - length;
+    let yY = Math.floor(Math.random() * (maxY + 1));
+    const xX = Math.floor(Math.random() * 10);
+
+    // Vertical coordinate
+    const vertical = [];
+    while (vertical.length < length) {
+      vertical.push([xX, yY]);
+      yY++;
+    }
+
+    return Math.random() < 0.5 ? horizontal : vertical;
+  }
+
+  // Place ship on board ------------------------------------------------------------------------------
+  place(ship) {
+    let coordinate = this.rollCoordinate(ship.length);
+
+    // Check whether coordinate exists
+    while (
+      coordinate.some(([x, y]) => {
+        return this.occupied.some(([occupiedX, occupiedY]) => {
+          return x === occupiedX && y === occupiedY;
+        });
+      })
+    ) {
+      coordinate = this.rollCoordinate(ship.length);
+    }
+
+    // Save ship and its coordinate
+    this.occupied.push(...coordinate);
+    this.ships.set(ship, coordinate);
+  }
+
+  receiveAttack(coordinate) {
+    // Check whether coordinate is marked
+    if (
+      this.marked.some(([x, y]) => {
+        return x === coordinate[0] && y === coordinate[1];
+      })
+    ) {
+      return;
+    }
+
+    // Mark coordinate
+    this.marked.push(coordinate);
+
+    // Function findShip()
+    const findShip = (coordinate) => {
+      for (const [ship, coordinates] of this.ships) {
+        const found = coordinates.some(([x, y]) => {
+          return x === coordinate[0] && y === coordinate[1];
+        });
+
+        if (found) return ship;
+      }
+      return false;
+    };
+
+    // Check whether coordinate has ship
+    const ship = findShip(coordinate);
+
+    // Ship receive attack
+    if (ship) {
+      ship.hit();
+
+      return "hit";
+    } else return "missed";
+  }
+
+  allShipsSunk() {
+    for (const ship of this.ships.keys()) {
+      if (!ship.isSunk()) return false;
+    }
+
+    return true;
+  }
+}
+
+export default GameBoard;
