e7d9027436a04eef9e2ccd98856072ba88ec1994
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-08-13 16:17:33 +0800

    refactor: rename gameLogic to game

--- src/gameLogic/player.js
+++
@@ -1,9 +0,0 @@
-import GameBoard from "./gameboard.js";
-
-class Player {
-  constructor() {
-    this.gameBoard = new GameBoard();
-  }
-}
-
-export default Player;
--- src/gameLogic/player.test.js
+++
@@ -1,8 +0,0 @@
-import Player from "./player.js";
-import GameBoard from "./gameboard.js";
-
-test("player has a gameboard", () => {
-  const player = new Player();
-
-  expect(player.gameBoard).toBeInstanceOf(GameBoard);
-});
