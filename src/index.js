9228a86bcf060334eaa9869a0a053b4f8360f5c2
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-08-16 14:56:30 +0800

    deployment commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import { init } from "./game/game.js";
-import { renderShipList, renderPlayerBoard } from "./ui/dom.js";
-import "./ui/event.js";
-
-init();
-renderShipList();
-renderPlayerBoard();
