3b6069fecc16e4bd264e520a0e0cb96e33ca9842
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-08-16 14:51:59 +0800

    feat: init webpage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./style.css";
+import { init } from "./game/game.js";
+import { renderShipList, renderPlayerBoard } from "./ui/dom.js";
+import "./ui/event.js";
 
+init();
+renderShipList();
+renderPlayerBoard();
